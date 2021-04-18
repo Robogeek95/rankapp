f34abe20c8a7ae6f4098c17fa0ecc88cbc13019f
robogeek95 <azeezlukman95@gmail.com>
2021-04-18 16:24:25 +0100

    initial commit

---
+++ .history/src/components/users_20210418161421.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>Users:</h3>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: null
+    };
+  },
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      this.users = res.data;
+    });
+  }
+};
+</script>
+
+<style>
+h3 {
+  margin-bottom: 5%;
+}
+</style>
